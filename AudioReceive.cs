ce5ff929eef0aca052aaa698317205d50e8d6ab9
Jan Kolkmeier <jankolkmeier@gmail.com>
2018-11-15 20:42:49 +0100

    update & license

--- AudioReceive.cs
+++ AudioReceive.cs
@@ -1,4 +1,21 @@
-﻿using System.Collections;
+﻿/*
+This file is part of the OpenIMPRESS project.
+
+OpenIMPRESS is free software: you can redistribute it and/or modify
+it under the terms of the Lesser GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenIMPRESS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with OpenIMPRESS. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
@@ -40,53 +57,51 @@ namespace oi.plugin.audio {
 		}
 
 		private void ParseData(UDPConnector udpSource) {
-			byte[] data = udpSource.GetNewData();
+			OIMSG msg = udpSource.GetNewData();
 			int packetsThisFrame = 0;
 
-			while (data != null) {
+			while (msg != null && msg.data != null && msg.data.Length > 0) {
 				// Make sure there is data in the stream.
 				packetsThisFrame++;
-				if (data.Length != 0) {
 
-					int packetID = -1;
-					using (MemoryStream str = new MemoryStream(data)) {
-						using (BinaryReader reader = new BinaryReader(str)) {
-							packetID = reader.ReadInt32();
-						}
-					}
-
-					if (packetID == 7) { // audio packet
-						float[] samples;
-						int freq;
-						int chan;
-						AudioSerializer.Deserialize(data, out samples, out freq, out chan);
-						if (freq != -1 && chan != -1) {
-
-							if (clipFreq != freq || clipChan != chan) {
-								clipFreq = freq;
-								clipChan = chan;
-								aud.clip = AudioClip.Create("RemoteAudio",
-									clipLen, clipChan, freq, false);
-								lastSamplePos = 0;
-								aud.timeSamples = 0;
-							}
-
-							aud.clip.SetData(samples, lastSamplePos);
-
-							if (lastSamplePos > aud.timeSamples + 10000 ||
-							   (lastSamplePos < aud.timeSamples && lastSamplePos > 10000
-								&& aud.timeSamples < clipLen - 10000)) {
-								aud.timeSamples = lastSamplePos;
-							}
-							if (!aud.isPlaying) aud.Play();
-
-							lastSamplePos += samples.Length;
-							if (lastSamplePos >= clipLen)
-								lastSamplePos -= clipLen;
-						}
+				int packetID = -1;
+				using (MemoryStream str = new MemoryStream(msg.data)) {
+					using (BinaryReader reader = new BinaryReader(str)) {
+						packetID = reader.ReadInt32();
 					}
 				}
-				data = udpSource.GetNewData();
+
+				if (packetID == 7) { // audio packet
+					float[] samples;
+					int freq;
+					int chan;
+					AudioSerializer.Deserialize(msg.data, out samples, out freq, out chan);
+					if (freq != -1 && chan != -1) {
+
+						if (clipFreq != freq || clipChan != chan) {
+							clipFreq = freq;
+							clipChan = chan;
+							aud.clip = AudioClip.Create("RemoteAudio",
+								clipLen, clipChan, freq, false);
+							lastSamplePos = 0;
+							aud.timeSamples = 0;
+						}
+
+						aud.clip.SetData(samples, lastSamplePos);
+
+						if (lastSamplePos > aud.timeSamples + 10000 ||
+							(lastSamplePos < aud.timeSamples && lastSamplePos > 10000
+							&& aud.timeSamples < clipLen - 10000)) {
+							aud.timeSamples = lastSamplePos;
+						}
+						if (!aud.isPlaying) aud.Play();
+
+						lastSamplePos += samples.Length;
+						if (lastSamplePos >= clipLen)
+							lastSamplePos -= clipLen;
+					}
+				}
+				msg = udpSource.GetNewData();
 			}
 
 		}
